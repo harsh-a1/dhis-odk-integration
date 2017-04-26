6b21eca3aa6b4120e112ef074e3a3bb0187f039a
harsh-a1 <harsh.atal@gmail.com>
2017-04-26 18:27:30 +0530

    WIP: fetched form data from ODK

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -2,7 +2,7 @@
  * Created by harsh on 16/7/16.
  */
 
-exports.DHIS_URL_BASE = "http://ntdindia.org/lf";
+exports.DHIS_URL_BASE = "http://139.162.61.147/nieodk";
 
 
 exports.username = "admin";
@@ -13,6 +13,7 @@ exports.auth = "Basic " + new Buffer(exports.username + ":" + exports.password).
 
 exports.ODKURL_HOST = "http://112.133.207.124";
 exports.ODKURL_PATH = "/ODKAggregate/view/submissionList?formId=edfss_data_form";
+exports.ODKURL_PATH_DATA = "/ODKAggregate/view/downloadSubmission?formId=edfss_data_form[@version=null@uiVersion=null]/eDFSS_DataCollect";
 exports.ODKURL_PORT = "8080";
 exports.ODK_USERNAME = "NIE_admin";
 exports.ODK_PASSWORD = "NIE!@#abc";
