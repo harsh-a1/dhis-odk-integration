f3ce9b4580024fc135ac147e32e6bd66fb9adc1a
harsh-a1 <harsh.atal@gmail.com>
2017-04-26 14:54:47 +0530

    Basic ODK Authentication + Fetching of instance ids

---
+++ CONSTANTS.js
@@ -0,0 +1,18 @@
+/**
+ * Created by harsh on 16/7/16.
+ */
+
+exports.DHIS_URL_BASE = "http://ntdindia.org/lf";
+
+
+exports.username = "admin";
+exports.password = "district";
+exports.auth = "Basic " + new Buffer(exports.username + ":" + exports.password).toString("base64");
+
+//exports.ODKURL_INSTANCE_IDS = "http://112.133.207.124:8080/ODKAggregate/view/submissionList?formId=edfss_data_form"
+
+exports.ODKURL_HOST = "http://112.133.207.124";
+exports.ODKURL_PATH = "/ODKAggregate/view/submissionList?formId=edfss_data_form";
+exports.ODKURL_PORT = "8080";
+exports.ODK_USERNAME = "NIE_admin";
+exports.ODK_PASSWORD = "NIE!@#abc";
