1d8bc9ce62e079a5f65b1be4b58ef41fd4edd95f
harsh-a1 <harsh.atal@gmail.com>
2017-05-17 21:45:39 +0530

    Threshold work

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -18,10 +18,38 @@ exports.ODKURL_PORT = "8080";
 exports.ODK_USERNAME = "NIE_admin";
 exports.ODK_PASSWORD = "NIE!@#abc";
 
-exports.eventProgramStage = "jDDmmG7ZePy";
-exports.eventProgram = "sIEPsgABx0L";
+exports.eventProgramStage = "apqU6esBuwV";
+exports.eventProgram = "X2XfE0rYiwk";
+exports.trackedEntity = "MCPQUTHX1Ze";
+exports.teiUID_TEA = "ysgOCWco2Gw";
 exports.ouODKKey = "district";
 exports.codePrefix = "/eDFSS_DataCollect/";
 exports.eventDateKey = "Date";
 exports.eventUIDKey = "instanceID";
 exports.deUIDCoordinate = "mEWZIvuQWwe";
+exports.DE_AFI_CASE = "xYO8HkDDW3N";
+exports.CLUSTER_DE_VILLAGE = "mEWZIvuQWwe";
+
+
+exports.CLUSTER_TEA_LABCONFIRMED = "gPGTS8eGa4i";
+exports.CLUSTER_TEA_3AFICASE = "oqTYHlWrWBh";
+exports.CLUSTER_TEA_5AFICASE = "oDg3FLcVw0R";
+exports.CLUSTER_TEA_2ADDCASE = "k3C0dkjcSg2";
+exports.CLUSTER_TEA_IS_ACTIVE = "sP8CfjSrtRq";
+exports.CLUSTER_TEA_ODKINTEGRATIONCODE = "AqHMFVqkwOG";
+exports.CLUSTER_TEA_COORDINATE = "ALvy8yTD1Np";
+exports.CLUSTER_TEA_FEATURETYPE = "puso6mGwJFL";
+exports.CLUSTER_PROGRAM = "wXXVzDXRLMe";
+exports.CLUSTER_RELATIONSHIP = "Foh90NcLrpq";
+exports.LAB_DE_GROUP = "YnLMC0T8BKB";
+
+
+
+
+exports.DHIS_Registration_Program = "sIEPsgABx0L";
+exports.DHIS_Registration_ProgramStage = "jDDmmG7ZePy";
+exports.DHIS_Registration_Village_DE = "mEWZIvuQWwe";
+exports.DHIS_Registration_Origin_DE = "CPrDolo2qSh";
+exports.DHIS_Registration_Syndrome_DE = "xYO8HkDDW3N";
+
+
